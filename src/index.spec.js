af0a29b20badb927ed0ac65ca3fee1bc93b80632
Robert Dey <robdey88@gmail.com>
2017-04-09 17:12:43 +0200

    extended unit-tests

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,5 +1,9 @@
+'use strict';
 var bst = require('bespoken-tools');
-var assert = require('assert');
+var chai = require('chai');
+
+var assert = chai.assert;
+chai.use(require('chai-string'));
 
 var server = null;
 var alexa = null;
@@ -31,11 +35,54 @@ afterEach(function(done) {
 it('Launches and tells user to start the game', function (done) {
     // Launch the skill via sending it a LaunchRequest
     alexa.launched(function (error, payload) {
-        // Check that the introduction is play as outputSpeech
+        // Check that the introduction is played as outputSpeech
         assert.equal(
           payload.response.outputSpeech.ssml, 
           '<speak> Schön, dass du mit mir spielen willst. Du fängst an, was nimmst du mit? </speak>'
         );
         done();
+    });
+});
+
+it('Repeats the user´s first item and adds a new one', function(done) {
+    alexa.spoken('Ich packe meinen Koffer und nehme mit. {Plastikpalme}', function (error, payload) {
+      const items = payload.sessionAttributes.items;
+      const newItem = items[items.length -1];
+
+      assert.equal(
+        payload.response.outputSpeech.ssml,
+        '<speak> Ich packe meinen Koffer und nehme mit. Plastikpalme und ' + newItem + ' </speak>'
+      );
+      done();
+    });
+});
+
+it('Tells the user which items he has forgotten', function(done) {
+    alexa.spoken('Ich packe meinen Koffer und nehme mit. {Plastikpalme}', function (error, payload) {
+      const items = payload.sessionAttributes.items;
+      const newItem = items[items.length -1];
+
+      alexa.spoken('Ich packe meinen Koffer und nehme mit. {Plastikpalme} und {Kleinkram}', function (error, payload) {
+        assert.equal(
+          payload.response.outputSpeech.ssml,
+          '<speak> Leider war das nicht richtig. Du hast ' + newItem + ' vergessen. Du hast damit verloren. </speak>'
+        );
+        done();
+      });
+    });
+});
+
+it('Tells the user which items were unneccessary', function(done) {
+    alexa.spoken('Ich packe meinen Koffer und nehme mit. {Plastikpalme}', function (error, payload) {
+      const items = payload.sessionAttributes.items;
+      const newItem = items[items.length -1];
+
+      alexa.spoken('Ich packe meinen Koffer und nehme mit. {Plastikpalme Kleinkram '+ newItem +'} und {Krimi}', function (error, payload) {
+        assert.equal(
+          payload.response.outputSpeech.ssml,
+          '<speak> Leider war das nicht richtig. Kleinkram hat niemand eingepackt. Du hast damit verloren. </speak>'
+        );
+        done();
+      });
     });
 });
