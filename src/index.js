3fab149f0e166f57afbb8a009c2982ff780e9919
Robert Dey <robdey88@gmail.com>
2017-04-09 18:31:59 +0200

    reverted app id fix

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ const APP_ID = 'amzn1.ask.skill.480b7029-32cb-4d29-9a9c-dac3d9c4316e';
 
 exports.handler = function(event, context, callback) {
     let alexa = Alexa.handler(event, context);
-    alexa.appId = APP_ID;
+    alexa.APP_ID = APP_ID;
     alexa.registerHandlers(handlers);
     alexa.execute();
 };
@@ -80,7 +80,7 @@ let handlers = {
     },
     'SessionEndedRequest': function() {
         console.log('SessionEndedRequest');
-        
+
         this.emit(':tell', 'Danke fürs Spielen.');
     }
 };
